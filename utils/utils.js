309210c19c18003a1aed76eda93d39355f4baf5b
Rooban K. <1367@smartcliff.loc>
2024-05-09 15:52:42 +0530

    change port

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 export const getAPIURL = () => {
   return process.env.NODE_ENV === "production"
-    ? "https://app.claritynotes.io"
+    ? "http://localhost:5353"
     : "https://smartcliff-server.onrender.com";
 };
