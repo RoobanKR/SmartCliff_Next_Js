eacdcd788b5f098faea780c829e0af42bf1add1c
Rooban K. <1367@smartcliff.loc>
2024-05-09 15:40:59 +0530

    change utill file

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 export const getAPIURL = () => {
   return process.env.NODE_ENV === "production"
-    ? "https://smartcliff-server.onrender.com"
-    : "https://smartcliff-server.onrender.com" || "http://localhost:5353";
+    ? "https://app.claritynotes.io"
+    : "https://smartcliff-server.onrender.com";
 };
