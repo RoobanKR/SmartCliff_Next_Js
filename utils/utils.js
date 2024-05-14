52944589febbad31bcfe4713e82051a3be4322b5
Rooban K. <1367@smartcliff.loc>
2024-05-14 10:13:17 +0530

    modify the util URL

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 export const getAPIURL = () => {
   return process.env.NODE_ENV === "production"
     ? "https://smartcliff-server-5qpp.onrender.com"
-    : "http://localhost:5353";
+    : "https://smartcliff-server-5qpp.onrender.com";
 };
