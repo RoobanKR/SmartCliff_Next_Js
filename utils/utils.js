d02fc73cbef6c9dc4b7702725513ab003e80da0f
Rooban K. <1367@smartcliff.loc>
2025-02-19 13:53:20 +0530

    change domain

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 export const getAPIURL = () => {
   return process.env.NODE_ENV === "production"
-    ? "https://smartcliff-server.onrender.com"
+    ? "https://smartcliff-server-5qpp.onrender.com"
     : "http://localhost:5353";
 };
