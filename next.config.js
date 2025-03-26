1da76f593b5385db50b8b8fd3f4a4c9067f05367
Rooban K. <1367@smartcliff.loc>
2025-03-26 10:07:00 +0530

    modify the next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "entsmkkdetemmddoukcj.supabase.co"],
+    domains: [
+      "localhost",
+      "entsmkkdetemmddoukcj.supabase.co",
+      "smartcliff-server-5qpp.onrender.com",
+    ],
   },
 };
-
+ 
 module.exports = nextConfig;
+ 
+ 
