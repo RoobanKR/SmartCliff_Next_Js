f12f625d389539346a7f03ba5b8d1b55ad8c660c
RoobanKR5 <55377839+RoobanKR@users.noreply.github.com>
2025-11-24 15:28:40 +0530

    Update the Leadfedder

--- app/page.jsx
+++ app/page.jsx
@@ -1,308 +1,108 @@
 "use client";
-import Preloader from "@/components/common/Preloader";
-import HeaderTwo from "@/components/layout/headers/HeaderTwo";
-import FooterTwo from "@/components/layout/footers/Footer";
-import HeroTwo from "@/components/homes/heros/HeroTwo";
-import FrequentlyAskedQuestion from "@/components/homes/faq/Faq";
-import Hiring from "@/components/homes/heros/Hiring";
-import TestimonialsEight from "@/components/common/testimonial8";
-import ExecutionOvervirewHome from "@/components/homes/heros/executionOverview";
-import CategoriesHomeOne from "@/components/homes/heros/serviceOverview";
-import WhySmartcliff from "@/components/homes/about/whysmartcliff";
-import ExecutiveOverview2 from "@/components/homes/heros/executionOverview2";
-import { useEffect, useRef, useState } from "react";
-import { ArrowLeft, ArrowRight, X } from "@mui/icons-material";
-import { useDispatch, useSelector } from "react-redux";
-import { getPopUpNotification } from "@/redux/slices/popUp/popUp";
-import { FaTimes } from "react-icons/fa";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Autoplay, Navigation, Pagination } from "swiper";
-import HomeClient from "@/components/homes/client/homeClient";
+import React, { useEffect } from "react";
+import Menu from "../component/Menu";
+import MobileMenu from "../component/MobileMenu";
+import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
+import Script from "next/script";
 
-export default function HomePage() {
-  const dispatch = useDispatch();
-  const { popupData, loading, error } = useSelector(
-    (state) => state.popupNotification
-  );
-  const [showPopup, setShowPopup] = useState(false);
-  const [openPopups, setOpenPopups] = useState([]);
-  const prevRef = useRef(null);
-  const nextRef = useRef(null);
+export default function HeaderTwo({ onServiceSelect }) {
+    const [activeMobileMenu, setActiveMobileMenu] = useState(false);
+    const [bgColor, setBgColor] = useState("#5b2c6f"); // Initially white
 
-  useEffect(() => {
-    dispatch(getPopUpNotification());
-  }, [dispatch]);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setBgColor("#5b2c6f"); // Change color when scrolled down
+            } else {
+                setBgColor("#5b2c6f"); // Revert to white when at the top
+            }
+        };
 
-  useEffect(() => {
-    if (popupData && popupData.length > 0) {
-      const filtered = popupData.filter((popup) => popup.isOpen);
-      setOpenPopups(filtered);
-      setShowPopup(filtered.length > 0);
-    }
-  }, [popupData]);
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll); // Cleanup
+    }, []);
 
-  const closePopup = () => {
-    setShowPopup(false);
-  };
+    // Handler function to receive service data from Menu
+    const handleServiceSelect = (service) => {
+        if (onServiceSelect) {
+            onServiceSelect(service);
+        }
+    };
 
-  return (
-    <>
-      {loading && <div>Loading...</div>}
-      {error && <div>Error: {error}</div>}
-
-      {openPopups.length > 0 && showPopup && (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100vw",
-            height: "100vh",
-            backgroundColor: "rgba(0, 0, 0, 0.6)",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            zIndex: 9999,
-          }}
+   return (
+  <>
+    <Script id="leadfeeder-script">
+      {`(function(ss,ex){
+        window.ldfdr = window.ldfdr || function(){
+          (ldfdr._q = ldfdr._q || []).push([].slice.call(arguments));
+        };
+        (function(d,s){
+          var fs = d.getElementsByTagName(s)[0];
+          function ce(src){
+            var cs = d.createElement(s);
+            cs.src = src;
+            cs.async = 1;
+            fs.parentNode.insertBefore(cs, fs);
+          };
+          ce('https://sc.lfeeder.com/lftracker_v1_' + ss + (ex ? '_' + ex : '') + '.js');
+        })(document, 'script');
+      })('3P1w24dKwoP7mY5n');`}
+    </Script>
+        <header
+            className="header -type-5 js-header"
+            style={{ background: bgColor }}
         >
-          <div
-            style={{
-              backgroundImage: openPopups[0].image
-                ? `url('${openPopups[0].image}')`
-                : "url('/assets/img/about-1/bckimg13.webp')",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-              borderRadius: "16px",
-              padding: "24px",
-              maxWidth: "1000px",
-              width: "95%",
-              maxHeight: "85vh",
-              overflowY: "auto",
-              position: "relative",
-              boxShadow: "0 8px 20px rgba(0, 0, 0, 0.5)",
-              border: "1px solid rgba(255, 255, 255, 0.1)",
-              color: "#ffffff",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              gap: "16px",
-            }}
-          >
-            {/* Close Button */}
-            <button
-              onClick={closePopup}
-              style={{
-                position: "absolute",
-                top: "-4px",
-                right: "0px",
-                background: "none",
-                border: "none",
-                padding: "8px",
-                borderRadius: "50%",
-                cursor: "pointer",
-                color: "red",
-                zIndex: 10000,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-              aria-label="Close"
-            >
-              <FaTimes size={26} style={{ pointerEvents: "none" }} />
-            </button>
-
-            {/* Swiper Content */}
-            <div style={{ width: "100%" }}>
-              {openPopups.length > 1 ? (
-                <Swiper
-                  modules={[Pagination, Navigation, Autoplay]}
-                  spaceBetween={20}
-                  slidesPerView={1}
-                  onInit={(swiper) => {
-                    if (swiper.params.navigation) {
-                      swiper.params.navigation.prevEl = prevRef.current;
-                      swiper.params.navigation.nextEl = nextRef.current;
-                      swiper.navigation.init();
-                      swiper.navigation.update();
-                    }
-                  }}
-                  pagination={{
-                    el: ".executive-pagination",
-                    clickable: true,
-                  }}
-                  autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                  }}
-                >
-                  {openPopups.map((popup, index) => (
-                    <SwiperSlide key={popup._id || index}>
-                      <PopupCard popup={popup} />
-                    </SwiperSlide>
-                  ))}
-                </Swiper>
-              ) : (
-                <PopupCard popup={openPopups[0]} />
-              )}
+            <div className="header__container" style={{ zIndex: 1000 }}>
+                <div className="row justify-between items-center">
+                    <div className="col-auto">
+                        <div className="header-left d-flex items-center">
+                            <div className="header__logo ">
+                                <Link href="/">
+                                    <Image
+                                        width={200}
+                                        height={70}
+                                        src="/assets/img/general/logo1.webp"
+                                        alt="logo"
+                                    />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-auto">
+                        <div className="header-right d-flex items-center">
+                            <div className="header-right__icons text-white d-flex items-center">
+                                <Menu
+                                    allClasses={"menu__nav text-dark-1 -is-active"}
+                                    onServiceSelect={handleServiceSelect}
+                                />
+                                <MobileMenu
+                                    setActiveMobileMenu={setActiveMobileMenu}
+                                    activeMobileMenu={activeMobileMenu}
+                                />
+                            </div>
+                            <div className="header-right__icons  d-flex items-center">
+                                <div className="d-none xl:d-block ml-20">
+                                    <button
+                                        onClick={() => setActiveMobileMenu(true)}
+                                        className="text-dark-1 items-center"
+                                        data-el-toggle=".js-mobile-menu-toggle"
+                                    >
+                                        <i className="text-11 icon icon-mobile-menu"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div
+                                className="header-right__buttons d-flex items-center ml-30 xl:ml-20 md:d-none"
+                                style={{ fontFamily: "serif" }}
+                            ></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            {/* Navigation and Pagination */}
-            {openPopups.length > 1 && (
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  gap: "20px",
-                  marginTop: "10px",
-                }}
-              >
-                <button
-                  ref={prevRef}
-                  style={{
-                    backgroundColor: "rgba(255, 255, 255, 0.1)",
-                    border: "none",
-                    padding: "6px",
-                    borderRadius: "9999px",
-                    cursor: "pointer",
-                    color: "black",
-                  }}
-                  aria-label="Previous"
-                >
-                  <ArrowLeft size={24} />
-                </button>
-
-                <div
-                  className="executive-pagination"
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    gap: "8px",
-                  }}
-                />
-
-                <button
-                  ref={nextRef}
-                  style={{
-                    backgroundColor: "rgba(255, 255, 255, 0.1)",
-                    border: "none",
-                    padding: "6px",
-                    borderRadius: "9999px",
-                    cursor: "pointer",
-                    color: "black",
-                  }}
-                  aria-label="Next"
-                >
-                  <ArrowRight size={24} />
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
-      )}
-
-      {/* Inline Script Tag */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('3P1w24dKwoP7mY5n');`
-        }}
-      />
-
-      {/* Page content */}
-      <Preloader />
-      <HeaderTwo />
-      <div className="content-wrapper js-content-wrapper overflow-hidden">
-        <HeroTwo />
-        <ExecutionOvervirewHome />
-        <ExecutiveOverview2 />
-        <WhySmartcliff />
-        <CategoriesHomeOne />
-        <HomeClient />
-
-        <TestimonialsEight />
-        <Hiring />
-        <FrequentlyAskedQuestion />
-        <FooterTwo />
-      </div>
-    </>
-  );
+        </header>
+           </>
+    );
 }
 
-function PopupCard({ popup }) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        height: "100%",
-        padding: "0 10px",
-      }}
-    >
-      <h2
-        style={{
-          fontSize: "20px",
-          fontWeight: "700",
-          color: "#9c3a7c",
-          marginBottom: "8px",
-          textAlign: "center",
-        }}
-      >
-        {popup.title}
-      </h2>
-      <p
-        style={{
-          color: "black",
-          fontSize: "16px",
-          lineHeight: "1.6",
-          textAlign: "center",
-          marginBottom: popup.link ? "12px" : "0",
-        }}
-      >
-        {popup.description}
-      </p>
-      {popup.link && (
-        <div style={{ textAlign: "center" }}>
-          <a
-            href={popup.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              display: "inline-block",
-              padding: "8px 20px",
-              backgroundColor: "#9c3a7c",
-              color: "#000",
-              fontWeight: "600",
-              fontSize: "14px",
-              borderRadius: "6px",
-              textDecoration: "none",
-              transition: "all 0.3s ease",
-              boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
-              marginTop: "12px",
-              border: "1px solid rgba(0,0,0,0.1)",
-              textAlign: "center",
-              minWidth: "120px",
-            }}
-            onMouseOver={(e) => {
-              e.target.style.backgroundColor = "#EAB308";
-              e.target.style.transform = "translateY(-1px)";
-              e.target.style.boxShadow = "0 4px 6px rgba(0,0,0,0.15)";
-            }}
-            onMouseOut={(e) => {
-              e.target.style.backgroundColor = "#9c3a7c";
-              e.target.style.transform = "translateY(0)";
-              e.target.style.boxShadow = "0 2px 4px rgba(0,0,0,0.1)";
-            }}
-          >
-            {popup.button}
-          </a>
-        </div>
-      )}
-    </div>
-  );
-}
